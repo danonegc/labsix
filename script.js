6f9aeb7775ad17d593d2425d7cdf919dad643062
danonegc <dcureton@gmail.com>
2017-07-06 02:49:47 -0400

    Updated tempconverterfunction lab

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-alert ('hi');
-function convertTemp(newTemp1,newTemp2){
 
-  if (newTemp1 === 100) {
-        console.log (newTemp1 * 9/5 + 32);
-  }
-  else if (newTemp1 === -40) {
-      console.log(newTemp1 * 9/5 + 32);
-  }
-  else {
-    console.log(((newTemp1 - 32) / (9/5)));
-  }
-}
+alert ("Welcome back to the Temperature Converter!")
+
+var userTemp = prompt('What is your temperature?')
+var userDegree = prompt('What degree scale...Fahrenheit or Celsius?')
 
 
-convertTemp(212,'');
-convertTemp(32,'');
-convertTemp(65,'');
-convertTemp(100,'');
-convertTemp(-40,'');
+function convertTemp(userTemp,userDegree){
+
+
+  if (userDegree === "Celsius") {
+        console.log ((userTemp + "\u00B0" + "C" + "  " + (userTemp * 9/5 + 32).toFixed(1)) + "\u00B0" + "F");
+  }   else if (userDegree === "Fahrenheit") {
+        console.log ((userTemp + "\u00B0" + "F" + " " + (userTemp-32)*(5/9)) + "\u00B0" + "C");
+  }   else {
+        console.log ("Select again please.");
+  }
+
+  }
+convertTemp(userTemp,userDegree); 
